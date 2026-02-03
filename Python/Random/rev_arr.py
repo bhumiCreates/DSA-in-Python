2c54d61d2aaf3fe682b68881a54b1d2a992ce4b0
Bhumi <bhumiwadhwani.work@gmail.com>
2026-02-04 02:34:06 +0530

    trashed files

--- Python/Random/rev_arr.py
+++
@@ -1,8 +0,0 @@
-arr = list(map(int, input("Enter array elements: ").split()))  #input array elements
-
-def reverse_array(arr):
-    if len(arr) == 0:
-        return []
-    return [arr[-1]] + reverse_array(arr[:-1])
-
-print(reverse_array(arr))
